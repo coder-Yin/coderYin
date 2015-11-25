10827a6c001ad9ad29205d2f5cf1f27172b27b8d
coder-Yin <yin_x_f@163.com>
2015-11-25 14:15:45 +0800

    fix

--- mysite/js/pages/index.js
+++ mysite/js/pages/index.js
@@ -1,3 +1,14 @@
+$(function(){
+
+	//判断浏览器的版本，不符合要求则跳转至提示页面
+	if(isLowerBrowser()){
+		window.location="notice.html";
+	}
+
+	//统计首页被访问的次数
+	getVisitNum();
+});
+
 function isLowerBrowser(){
 	var flag=false;
 
@@ -18,6 +29,17 @@ function isLowerBrowser(){
 	return flag;
 }
 
-if(isLowerBrowser()){
-	window.location="notice.html";
+function getVisitNum(){
+	var storage = window.localStorage;
+	if(storage){
+		if (!storage.getItem("pageLoadCount")){
+			storage.setItem("pageLoadCount",0);
+		}
+		storage.pageLoadCount = parseInt(storage.getItem("pageLoadCount")) + 1;//必须格式转换
+		console.log("首页已被访问："+storage.pageLoadCount+" 次，谢谢您的访问~");
+	}
 }
+
+
+
+
